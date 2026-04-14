3fc15ccb703b115faff65651988f1f8d980b9144
Moiz Ansari <143032330+MoizAnsari90@users.noreply.github.com>
2026-04-15 01:05:43 +0500

    Updates

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: "https://nex-ride-two.vercel.app/",
+    origin: "https://nex-ride-two.vercel.app",
     credentials: true
 }));
 app.use(cookieparser());
