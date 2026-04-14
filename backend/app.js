66c205b6b55c8a2032d67a27e29ffb04cee7378e
Moiz Ansari <143032330+MoizAnsari90@users.noreply.github.com>
2026-04-15 00:44:16 +0500

    Update CORS

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: "https://nex-ride-two.vercel.app/",
     credentials: true
 }));
 app.use(cookieparser());
