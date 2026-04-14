179bffcfa294fc463c27e942faf62d607ecebff6
Abdul Moiz Ansari <moizansariak@gmail.com>
2026-04-15 01:52:36 +0500

    fix

--- frontend/src/components/GoogleMap.jsx
+++ frontend/src/components/GoogleMap.jsx
@@ -1,6 +1,8 @@
+
 import React, { useEffect, useRef, useState } from 'react'
 
-export const GOOGLE_MAPS_API_KEY = 'AIzaSyDW2xmljnEqNkNx79lDxBl8uqfQ6QUEg2I'
+export const GOOGLE_MAPS_API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+
 
 let scriptLoadPromise = null
 
